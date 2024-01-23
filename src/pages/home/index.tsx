7f2b8c64d4e064b03bb2fcfd3ac0f1c9043a6955
金子扬 <1528249383@qq.com>
2024-01-23 16:24:56 +0800

    save

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@/components/hoc/with-auth';
 //import { useLang } from '@/components/hoc/with-lang/define';
-import { Progress, Space } from '@nutui/nutui-react-taro';
+import background from '@/assets/xueli.png';
+import { Image, Progress, Space } from '@nutui/nutui-react-taro';
 import { View } from '@tarojs/components';
 import styles from './index.module.scss';
 
@@ -13,17 +14,29 @@ const Home = () => {
       <View className={styles.topCodingExp}>{user?.coding}</View>
       <View className={styles.userStatus}>
         <Space>
-          <view className={styles.leftProgress}>
+          <View className={styles.leftProgress}>
             <Space direction="vertical">
               <View>{'今日学习'}</View>
-              <view className={styles.staticsNumber}>
+              <View className={styles.staticsNumber}>
                 <span className={styles.mainNumber}>{20}</span>
                 <span className={styles.subffix}>/ 60分钟</span>
-              </view>
+              </View>
               <Progress percent={30} background="#fff" color="rgb(46,48,48)" strokeWidth="5" />
             </Space>
-          </view>
+          </View>
+          <View className={styles.right}>
+            <Space direction="vertical">
+              <View className={styles.days}>
+                <span className={styles.mainNum}>30</span>
+                <span>天</span>
+              </View>
+              <View>{'已连续学习'}</View>
+            </Space>
+          </View>
         </Space>
+      </View>
+      <View className={styles.imgContainer}>
+        <Image src={background} width="200" height="200" className={styles.img} />
       </View>
     </View>
   );
