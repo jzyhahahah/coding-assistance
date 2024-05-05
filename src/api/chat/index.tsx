99b430a0974bff3c431073e7e5a811435e22ad2b
jinziyang <1528249383@qq.com>
2024-05-05 22:12:35 +0800

    feat: 更新token

--- src/api/chat/index.tsx
+++ src/api/chat/index.tsx
@@ -6,7 +6,7 @@ export const useChat = () => {
   return useRequest(
     async (params: ChatParams) => {
       const res = await Taro.request({
-        url: 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/ernie-3.5-8k-0205?access_token=24.4fb883402d35e62e4a14b48690fad812.2592000.1715348621.282335-60744639',
+        url: 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/ernie-3.5-8k-0205?access_token=24.d01e05060b51e8ca1e5f187d539241ec.2592000.1717510262.282335-60744639',
         method: 'POST',
         data: params
       });
